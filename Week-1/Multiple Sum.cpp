295b0b4bf4d0146af1160fdfda45b717806ab60c
ankityadav121 <87987810+ankityadav121@users.noreply.github.com>
2022-05-26 22:41:48 +0530

    Multiple Sum.cpp

--- Week-1/Multiple Sum.cpp
+++ Week-1/Multiple Sum.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+long long int multipleSum(int x){
+  // Write your code here
+   long long n1,n2,n3,res=0;
+  n1=x/3,n2=x/5;n3=x/15;
+  res+=(n1*(n1+1))/2*3;
+    res+=(n2*(n2+1))/2*5;
+    res-=(n3*(n3+1))/2*15;
+  return res;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        cout << multipleSum(x) << "\n";
+    }
+    return 0;
+}
